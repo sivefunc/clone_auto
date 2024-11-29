ddb9f3d7463ceb9dc5a08d268771d361744a39df
sivefunc <sivefunc@tuta.io>
2024-11-29 13:25:42 -0400

    feaT: exception handling

--- src/clone_auto.py
+++ src/clone_auto.py
@@ -8,8 +8,14 @@ def get_gitlab_repos(user: str, *args, **kwargs) -> list:
     """Public Gitlab Repositories from a user
     """
     api = "https://gitlab.com/api/v4/users/USER/projects"
-    response = requests.get(api.replace("USER", user), *args, **kwargs)
-    response.raise_for_status()
+    try:
+        response = requests.get(api.replace("USER", user), *args, **kwargs)
+        response.raise_for_status()
+    
+    except requests.exceptions.RequestException as error:
+        print(error)
+        return []
+
     user_json = response.json()
 
     repos = [repo['http_url_to_repo'] for repo in user_json]
