caa1487a99da917a012c96ff161ab8f24d7c7862
sivefunc <sivefunc@tuta.io>
2024-11-28 22:27:23 -0400

    feat: clone repo

--- src/clone_auto.py
+++ src/clone_auto.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import requests
 
 from _version import __version__
@@ -13,9 +15,18 @@ def get_gitlab_repos(user: str, *args, **kwargs) -> list:
     repos = [repo['http_url_to_repo'] for repo in user_json]
     return repos
 
+def clone_repo(git_url: str) -> bool:
+    """git clone git_url
+    """
+    command = ["git", "clone", git_url]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+    for c in iter(lambda: process.stdout.read(1), b""):
+        print(c.decode('ascii'), end="")
+
 def main():
     for repo in get_gitlab_repos('sivefunc'):
-        print(repo)
+        clone_repo(repo)
 
 if __name__ == '__main__':
     main()
